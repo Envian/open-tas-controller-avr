a31d50b7368965252425bb98fd40a2c03e5b59cc
Russell Small <Envian@users.noreply.github.com>
2019-06-21 20:39:34 +0000

    Updates to convert script, added a flash cart injector

--- scripts/convert_mupen.py
+++ scripts/convert_mupen.py
@@ -1,4 +1,4 @@
-# Convert Mupen - Converts MUPEN input files into TASRAW files.
+# Converts emulator recording files into TASRAW files.
 # Copyright (C) 2019  Russell Small
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,54 +15,23 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
+from tasrawStream import TasrawStream
 
 #Note: This ignores the header, and thus version of the MUPEN recording. Will not work with multi-controller tas's,
 # though it would still produce a tas file.
-
-HEADER = b"TASRAW"
-CONSOLE = 0x64
-VERSION = 0
-
 
 def usage():
 	print("convert_mupen [file] [desctription]")
 	print("outputs inputs.inp in the correct format. Description is optional")
 
 def main(input, description):
-	descriptionBytes = bytearray(32)
-	if description:
-		if len(description) > 32:
-			description = description[:32]
-		descriptionBytes = description.encode("ASCII", "replace").ljust(32, bytearray(1))
-
-	with open(input, "rb") as source, open("inputs.inp", "wb") as target:
-		#Standard Header
-		target.write(b"TASRAW")
-		target.write(bytearray([CONSOLE, VERSION]))
-		target.write(descriptionBytes)
-
+	with open(input, "rb") as source, TasrawStream("inputs.inp", description) as target:
 		source.seek(0x400)
-		current = source.read(4)
-		count = 0
-
 		while True:
 			bytes = source.read(4)
-
 			if len(bytes) != 4:
-				break
-
-			if bytes != current or count == 255:
-				target.write(current)
-				target.write(bytearray([count]))
-				current = bytes
-				count = 0
-			else:
-				count += 1
-
-		target.write(current)
-		target.write(bytearray([count]))
-		target.flush()
-
+				return
+			target.writeInput(bytes)
 
 if len(sys.argv) == 2:
 	main(sys.argv[1], None)
